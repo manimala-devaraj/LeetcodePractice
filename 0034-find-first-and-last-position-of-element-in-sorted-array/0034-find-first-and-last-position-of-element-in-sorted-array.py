9b0693e9cdeb4eb2aa6e90d9571eb1f2bc9eff44
manimala-devaraj <manimaladevaraj@gmail.com>
2025-06-24 13:04:30 +0530

    Time: 0 ms (100%), Space: 19.1 MB (52.89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,53 +1,32 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        def binary_search(nums, target, is_searching_left):
-            left = 0
-            right = len(nums) - 1
-            idx = -1
-            
-            while left <= right:
-                mid = (left + right) // 2
+        left = 0 
+        right = len(nums) - 1 
+        sol = []
+        while (left < right):
+            mid = int((right - left )/2 + left) 
+            if target <= nums[mid]:    
+                right = mid
+            else:
+                left = mid+1
+        sol.append(right)
+
+        left = 0 
+        right = len(nums) - 1 
                 
-                if nums[mid] > target:
-                    right = mid - 1
-                elif nums[mid] < target:
-                    left = mid + 1
-                else:
-                    idx = mid
-                    if is_searching_left:
-                        right = mid - 1
-                    else:
-                        left = mid + 1
-            
-            return idx
+        while (left < right):
+            mid = int((right - left +1)/2 + left)
+            if target >= nums[mid]:
+                left = mid 
+            else:
+                right = mid - 1
+        sol.append(left)
+       
+
+        if left>=0 and left < len(nums):
+            if nums[left] == target:
+                return sol
+        return [-1,-1]
+
+
         
-        left = binary_search(nums, target, True)
-        right = binary_search(nums, target, False)
-        
-        return [left, right]
-            
-
-
-
-
-# class Solution:
-#     def searchRange(self, nums: List[int], target: int) -> List[int]:
-#         nums.sort()
-#         l=len(nums)
-#         medi=l//2
-#         ra=nums[:medi]
-#         for i in ra:
-#             if i==target:
-#                 return (nums.index[i,i])
-#             else:
-#                 return [-1,-1]
-
-#         s=nums[medi:]
-#         for j in s:
-#             if j==target:
-#                 return (num.index[i,i])
-#             else:
-#                 return [-1,-1]
-
-            
