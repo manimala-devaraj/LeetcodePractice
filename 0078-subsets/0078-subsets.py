4bbab2eca39d65687b71d53ba638697645eb5674
manimala-devaraj <manimaladevaraj@gmail.com>
2025-06-24 12:14:54 +0530

    Time: 0 ms (100%), Space: 17.9 MB (40.25%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        def backtrack(start, path):
-            res.append(path[:])  
-            for i in range(start, len(nums)):
-                path.append(nums[i])           
-                backtrack(i + 1, path)         
-                path.pop()                     
-        backtrack(0, [])
-        return res
+        result = [[]]  
+        for i in nums:
+            new= []
+            for subset in result:
+                new.append(subset + [i])
+            result.extend(new)
+
+        return result
+
+        # n = len(nums)
+        # res = []
+        # for i in range(2**n): 
+        #     subset = []
+        #     for j in range(n):
+        #         if (i >> j) & 1:
+        #             subset.append(nums[j])
+        #     res.append(subset)
+        # return res
+
+        # res = []
+        # def backtrack(start, path):
+        #     res.append(path[:])  
+        #     for i in range(start, len(nums)):
+        #         path.append(nums[i])           
+        #         backtrack(i + 1, path)         
+        #         path.pop()                     
+        # backtrack(0, [])
+        # return res
