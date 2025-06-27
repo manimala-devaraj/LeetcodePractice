fdfd3226a8c7ff4901da91fde2672cf353fd4b79
manimala-devaraj <manimaladevaraj@gmail.com>
2025-06-27 17:06:31 +0530

    Time: 2272 ms (7.2%), Space: 19.2 MB (86.56%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -3,10 +3,16 @@ class Solution:
         st=[]
         n=len(nums)
         res=[-1]*n
-        for i in range(2 * n):
-            new= nums[i % n]
-            while st and nums[st[-1]] < new:
-                res[st.pop()] =new
-            if i < n:
-                st.append(i)
+        for i in range(n):
+            for j in range(1,n):
+                if nums[(i+j)%n]>nums[i]:
+                    res[i]=nums[(i+j)%n]
+                    break
         return res
+        # for i in range(2 * n):
+        #     new= nums[i % n]
+        #     while st and nums[st[-1]] < new:
+        #         res[st.pop()] =new
+        #     if i < n:
+        #         st.append(i)
+        # return res
