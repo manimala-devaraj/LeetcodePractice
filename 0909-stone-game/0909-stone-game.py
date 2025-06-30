b03d436c76dd623f9b1eb7adfe3cefc8bb69b2d5
manimala-devaraj <manimaladevaraj@gmail.com>
2025-06-30 17:09:33 +0530

    Time: 0 ms (100%), Space: 17.9 MB (61.45%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        Alice = []
+        Bob = []
+
+        while len(piles) > 0:
+            if piles[0] >= piles[-1]:
+                Alice.append(piles[0])
+                piles.remove(piles[0])
+                Bob.append(piles[-1])
+                piles.remove(piles[-1])
+            else:
+                Alice.append(piles[-1])
+                piles.remove(piles[-1])
+                Bob.append(piles[0])
+                piles.remove(piles[0])
+
+        if sum(Alice) > sum(Bob):
+            return True 
+        else:
+            return False
